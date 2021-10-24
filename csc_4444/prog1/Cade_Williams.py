4f6ec87fb37364e8e78e9cf8b1fada4e3ca32de5
Cade Williams <cadegw1@gmail.com>
2021-10-24 02:27:40 -0500

    csc_4444 prog1 fixed cost function and added comments

--- csc_4444/prog1/Cade_Williams.py
+++ csc_4444/prog1/Cade_Williams.py
@@ -3,20 +3,22 @@
 import numpy as np
 
 # Initializing environment with five dirty squares in the top row of the grid
-grid = np.array([[False, False, False, False, False],
-                [True, True, True, True, True],
-                [True, True, True, True, True],
-                [True, True, True, True, True],
-                [True, True, True, True, True]])
+env = np.array([[False, False, False, False, False],        # [0, 1, 2, 3, 4]
+                [True, True, True, True, True],             # [1, x, x, x, x]
+                [True, True, True, True, True],             # [2, x, x, x, x]
+                [True, True, True, True, True],             # [3, x, x, x, x]
+                [True, True, True, True, True]])            # [4, x, x, x, x]
 
 
 class Agent:
 
+    # initializes the agent at a position, time = 0, and cost = 0
     def __init__(self):
         self.position = [0, 4]  # (x, y) pair with 0-based columns and rows
         self.time = 0
         self.cost = 0
 
+    # method to traverse through the environment
     def move(self, direction):
         if direction == "Left" and self.position[0] != 0:
             self.position[0] -= 1
@@ -26,18 +28,21 @@ class Agent:
             self.position[1] -= 1
         elif direction == "Down" and self.position[1] != 4:
             self.position[1] += 1
+        else:
+            print("ERROR: Agent in unknown position")
         self.time += 1
-        self._cost
+        self.incur_cost()
 
     def suck(self):
-        grid[self.position[1]][self.position[0]] = True
+        env[self.position[1]][self.position[0]] = True     # env[y][x]
         self.time += 1
-        self._cost
+        self.incur_cost()
 
-    def _cost(self):
-        for row in grid:
-            for column in grid:
-                if not grid[column][row]:
+    # method to increment cost at the end of each time step
+    def incur_cost(self):
+        for row in env:
+            for element in row:
+                if not element:
                     self.cost += 2
 
 
@@ -48,5 +53,5 @@ if __name__ == "__main__":
     a.move("Up")
     a.move("Up")
     a.suck()
-    print(grid[0][1])
+    print(env[0][0], a.time, a.cost)
 
