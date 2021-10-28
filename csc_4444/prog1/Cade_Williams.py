ad43a2ab8cb2eb426eab57d4cf10ecbb7cdf8d63
Cade Williams <cadegw1@gmail.com>
2021-10-28 02:32:48 -0500

    csc_4444 prog1 added VacuumProblem and method to create graph if necessary

--- csc_4444/prog1/Cade_Williams.py
+++ csc_4444/prog1/Cade_Williams.py
@@ -3,95 +3,51 @@
 import search as s
 
 
-class FullyObservableVacuumProblem(s.Problem):
+class VacuumProblem(s.Problem):
 
-    def __init__(self, initial, g):
-        self.initial = initial
-        self.goal = g
+    def __init__(self, initial, goal=True):
+        super().__init__(initial, goal)
 
     def actions(self, state):
-        return ["Left", "Right", "Up", "Down", "Suck"]
+        x, y = state
+        possible_actions = ['Suck', 'Left', 'Right', 'Down', 'Up']
 
-    def result(self, state, agent_location, action):
-        x, y = agent_location
-        if x == 0 and action == "Left":
-            return state
-        elif x == 4 and action == "Right":
-            return state
-        elif y == 0 and action == "Down":
-            return state
-        elif y == 4 and action == "Up":
-            return state
-        elif action == "Suck" and state[agent_location] == 'Clean':
-            return state
-        else:
-            if action == "Left":
-                x -= 1
-            elif action == "Right":
-                x += 1
-            elif action == "Down":
-                y -= 1
-            elif action == "Up":
-                y += 1
-            elif action == "Suck":
-                state[agent_location] = 'Clean'
-        return state
+        if x-1 < 0:
+            possible_actions.remove('Left')
+        if x+1 > 4:
+            possible_actions.remove('Right')
+        if y-1 < 0:
+            possible_actions.remove('Down')
+        if y+1 > 4:
+            possible_actions.remove('Up')
+
+        return possible_actions
+
+    def result(self, state, action):
+        x, y = state
+        if action == "Left":
+            x -= 1
+        elif action == "Right":
+            x += 1
+        elif action == "Down":
+            y -= 1
+        elif action == "Up":
+            y += 1
+        elif action == "Suck":
+            # TODO: add something here
+            pass
+        return x, y
 
     def goal_test(self, state):
-        # is_goal = True
-        # for square in state:
-        #     if state[square] == 'Dirty':
-        #         is_goal = False
-        # return is_goal
+        is_goal = True
+        for square in state:
+            if state[square] == 'Dirty':
+                is_goal = False
+        return is_goal
         return state == self.goal
 
-    def path_cost(self, c, state1, action, state2):
-        for square in state2:
-            if state2[square] == 'Dirty':
-                c += 2
-        return c
-
-    def h(self, args):
+    def h(self, node):
         return 9
-
-    def value(self, state):
-        return
-
-
-class Agent:
-
-    # initializes the agent at a position, time = 0, and cost = 0
-    def __init__(self):
-        self.position = [0, 4]  # (x, y) pair with 0-based columns and rows
-        self.time = 0
-        self.cost = 0
-
-    # method to traverse through the environment
-    def move(self, direction):
-        if direction == "Left" and self.position[0] != 0:
-            self.position[0] -= 1
-        elif direction == "Right" and self.position[0] != 4:
-            self.position[0] += 1
-        elif direction == "Up" and self.position[1] != 0:
-            self.position[1] -= 1
-        elif direction == "Down" and self.position[1] != 4:
-            self.position[1] += 1
-        else:
-            print("ERROR: Agent in unknown position")
-        self.time += 1
-        self.incur_cost()
-
-    def suck(self):
-        env[self.position[1]][self.position[0]] = True     # env[y][x]
-        self.time += 1
-        self.incur_cost()
-
-    # method to increment cost at the end of each time step
-    def incur_cost(self):
-        for row in env:
-            for element in row:
-                if not element:
-                    self.cost += 2
 
 
 if __name__ == '__main__':
@@ -102,14 +58,28 @@ if __name__ == '__main__':
            (0, 2): 'Clean', (1, 2): 'Clean', (2, 2): 'Clean', (3, 2): 'Clean', (4, 2): 'Clean',
            (0, 1): 'Clean', (1, 1): 'Clean', (2, 1): 'Clean', (3, 1): 'Clean', (4, 1): 'Clean',
            (0, 0): 'Clean', (1, 0): 'Clean', (2, 0): 'Clean', (3, 0): 'Clean', (4, 0): 'Clean'}
+    action_list = ['Suck', 'Left', 'Right', 'Down', 'Up']
 
-    goal = {(0, 4): 'Clean', (1, 4): 'Clean', (2, 4): 'Clean', (3, 4): 'Clean', (4, 4): 'Clean',
-            (0, 3): 'Clean', (1, 3): 'Clean', (2, 3): 'Clean', (3, 3): 'Clean', (4, 3): 'Clean',
-            (0, 2): 'Clean', (1, 2): 'Clean', (2, 2): 'Clean', (3, 2): 'Clean', (4, 2): 'Clean',
-            (0, 1): 'Clean', (1, 1): 'Clean', (2, 1): 'Clean', (3, 1): 'Clean', (4, 1): 'Clean',
-            (0, 0): 'Clean', (1, 0): 'Clean', (2, 0): 'Clean', (3, 0): 'Clean', (4, 0): 'Clean'}
+    graph_dict = {}
+    for x in range(0, 5):
+        for y in range(0, 5):
+            next_states = {'Suck': [(x, y)], 'Left': [(x-1, y)], 'Right': [(x+1, y)],
+                           'Down': [(x, y-1)], 'Up': [(x, y+1)]}
+            # next_states = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+            graph_dict[(x, y)] = {}
+            for action in action_list:
+                tx, ty = next_states[action][0]
+                if tx < 0 or tx > 4 or ty < 0 or ty > 4:
+                    del next_states[action]
+                graph_dict[(x, y)] = next_states
 
-    problem = FullyObservableVacuumProblem(env, goal)
-    s.astar_search(problem, problem.h)
+    new_problem = VacuumProblem((0, 0))
+    print(s.astar_search(new_problem, new_problem.h))
+    #
+    # graph = s.Graph(graph_dict=graph_dict)
+    # problem = s.GraphProblem(initial=(0, 0), goal=True, graph=graph)
+    # print(s.astar_search(problem, problem.h))
+
+
 
 
