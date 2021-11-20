7e1f8345976c7ceb3042881e407f49ed00de110b
Cade Williams <cadegw1@gmail.com>
2021-11-20 01:24:36 -0600

    csc-4103 prog2 expanded write function

--- csc-4103/prog2/raid.c
+++ csc-4103/prog2/raid.c
@@ -10,14 +10,14 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define FILENAME_SIZE       100
+#define BUFFER_SIZE     4096
 
 // Function removes old disk files in the current directory 
 void clean_dir(void)
 {
     FILE *old_disk;
     int i = 0;
-    char filename[FILENAME_SIZE];
+    char filename[FILENAME_MAX];
     sprintf(filename, "disk.%d", i++);
     while(old_disk = fopen(filename, "r"))
     {
@@ -25,15 +25,19 @@ void clean_dir(void)
         remove(filename);
         sprintf(filename, "disk.%d", i++);
     } 
-    printf("Removed old disk files\r\n");
+    if(i != 1)
+    {
+        printf("Removed old disk files ...\r\n");
+    }
 }
+    
 
 // Creates all necessary files
 void create_files(int disk_amt)
 {
     // Create files
     FILE *disk[disk_amt];
-    char filename[FILENAME_SIZE];
+    char filename[FILENAME_MAX];
     for(int i = 0; i < disk_amt; i++)
     {
         sprintf(filename, "disk.%d", i);
@@ -42,56 +46,72 @@ void create_files(int disk_amt)
     }
 }
 
-void write(void)
+void write(int disk_amt, int block_size, char * input)
+{
+    // Initialize input data file
+    char buff[BUFFER_SIZE];
+    FILE *input_data;
+    input_data = fopen(input, "r");
+
+    // Initialize disk file array
+    FILE *disk[disk_amt];
+    char filename[FILENAME_MAX];
+
+    // Add way to know when to skip writing to calculate parity block
+    int i = 0;
+    while((fgets(buff, block_size + 1, input_data)) != NULL)
+    {
+        sprintf(filename, "disk.%d", i);
+        disk[i] = fopen(filename, "w");
+        // fprintf(disk);
+        fclose(disk[i]);
+    }
+    fclose(input_data);
+}
+
+void read(char * output)
 {
 
 }
 
-void read(void)
+void rebuild(char * disk)
 {
 
 }
-
-void rebuild(void)
-{
-
-}
-
-enum cmd{NUM_DISK = 1, BLOCK_SIZE = 2, COMMAND = 3, INPUT = 4};
 
 int main(int argc, char *argv[])
 {
     int num_disks = 0;
     int block_size = 0;
     char * command = "";
-    char * input = "";
+    char * file = "";
 
     // Retrieve command line arguments
+    clean_dir();
     if(argc != 5)
     {
-        printf("ERROR: Four arguments expected");
+        printf("ERROR: Four arguments expected\r\n");
     }
     else
     {
-        num_disks = atoi(argv[NUM_DISK]);
-        block_size = atoi(argv[BLOCK_SIZE]);
-        command = argv[COMMAND];
-        input = argv[INPUT];
-    }
+        num_disks = atoi(argv[1]);
+        block_size = atoi(argv[2]);
+        command = argv[3];
+        file = argv[4];
 
-    clean_dir();
-    create_files(num_disks);
-    if(argv[COMMAND] == "write")
-    {
-        write();
-    }
-    else if(argv[COMMAND] == "read")
-    {
-        read();
-    }
-    else if(argv[COMMAND] == "rebuild")
-    {
-        rebuild();
+        create_files(num_disks);
+        if(strcmp(command, "write") == 0)
+        {
+            write(num_disks, block_size, file);
+        }
+        else if(strcmp(command, "read") == 0)
+        {
+            read(file);
+        }
+        else if(strcmp(command, "rebuild") == 0)
+        {
+            rebuild(file);
+        }
     }
 
     return 0;
